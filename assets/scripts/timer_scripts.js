3fb1a830ce5c53aa4b4d3a352314447c77a93736
JuliusPohjanen <julius.pohjanen@gmail.com>
2023-10-13 17:51:22 +0300

    more progress

---
+++ assets/scripts/timer_scripts.js
@@ -0,0 +1,19 @@
+//functions for the timer
+// Check if the current page is not the front page before redirecting
+function redirectToFrontPage() {
+    if (window.location.href.indexOf('/FrontPage.html') === -1) {
+      window.location.href = '/FrontPage.html';
+    }
+  }
+  
+  function oneMinuteRemainingAlarm() {
+    window.alert("One minute remaining! Prepare to be redirected.");
+  }
+  
+  // Set a timeout for redirecting to the front page after 5 minutes
+  setTimeout(redirectToFrontPage, 300000);
+  
+  // Set an interval to show the one-minute remaining alert every 4 minutes
+  var oneMinuteInterval = setInterval(function () {
+    oneMinuteRemainingAlarm();
+  }, 240000);
